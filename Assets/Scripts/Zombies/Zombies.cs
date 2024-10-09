ad17d3dc67cd64c3f9818062d9dbbda97f806658
FumeGrandpa <victor.jeppsson@elev.ga.lbs.se>
2024-10-09 10:08:03 +0200

    Zombie skript
    La till skript för Zombies, som ska ge arv till alla enemies

---
+++ Assets/Scripts/Zombies/Zombies.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombies : MonoBehaviour
+{
+    protected float speed = 10f;
+    protected float damage = 10f;
+    protected float health = 10f;
+
+    protected bool isShooter = false;
+    protected bool isExplosive = false;
+}
