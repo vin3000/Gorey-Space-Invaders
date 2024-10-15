c8e193bdd60f93b972f30b6a0559ada042850e91
GrandpaMiles <milton.hernvald@elev.ga.lbs.se>
2024-10-15 10:31:39 +0200

    blood particle effect

--- Assets/Scripts/bloodparticle.cs
+++ Assets/Scripts/bloodparticle.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Collections.AllocatorManager;
 
-public class bloodeffect : MonoBehaviour
+public class bloodparticle : MonoBehaviour
 {
+
+    public ParticleSystem Bloodparticle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Instantiate(Bloodparticle);
     }
 }
+
